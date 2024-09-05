14d3b8ff1e7e7bd5f2a303171a9a6e85b3068473
Saulo Carvalho <saulo_carvalho123@hotmail.com>
2024-09-04 22:08:29 -0300

    Initial commit

---
+++ src/components/pages/Home/index.tsx
@@ -0,0 +1,23 @@
+import './styles.css'
+
+
+export default function Home() {
+
+
+  return (
+    <main>
+
+      <div className='container'>
+        <h1>Grade Average</h1>
+
+        <input type="text" />
+
+        <button>Somar</button>
+
+        <button>Calcular Media</button>
+
+      </div>
+
+    </main>
+  )
+}
